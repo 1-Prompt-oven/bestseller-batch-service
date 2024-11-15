5e1024942c929cb05236d93ad9550c2042755aa2
jaehunLee-git <joz526@naver.com>
2024-11-15 17:21:53 +0900

    First Commit

---
+++ src/main/java/com/promptoven/sellerbatchservice/SellerbatchserviceApplication.java
@@ -0,0 +1,13 @@
+package com.promptoven.sellerbatchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerbatchserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerbatchserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/promptoven/sellerbatchservice/SellerbatchserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promptoven.sellerbatchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerbatchserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
